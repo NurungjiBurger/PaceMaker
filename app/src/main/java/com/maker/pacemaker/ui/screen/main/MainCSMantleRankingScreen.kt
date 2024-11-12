7d49eb103c8eee1bdf1d4a075526e58b01fe4707
김세은 <kse08160@naver.com>
2024-11-12 12:00:59 +0900

    [#53] 싸맨틀 랭킹 구현 완료

--- app/src/main/java/com/maker/pacemaker/ui/screen/main/MainCSMantleRankingScreen.kt
+++ app/src/main/java/com/maker/pacemaker/ui/screen/main/MainCSMantleRankingScreen.kt
@@ -2,110 +2,126 @@ package com.maker.pacemaker.ui.screen.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.maker.pacemaker.R
 import com.maker.pacemaker.ui.viewmodel.main.details.MainCSMantleRankingScreenViewModel
 
 @Composable
 fun MainCSMantleRankingScreen(viewModel: MainCSMantleRankingScreenViewModel) {
-    Column(
+
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val userRank by viewModel.userRanks.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.showRanking() // 화면 표시 시 실행하고자 하는 함수 호출
+    }
+
+    ConstraintLayout(
+
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .background(color = Color(0xFFFAFAFA))
     ) {
-        // 상단 메달과 텍스트
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        ) {
-            Text(
-                text = "순위를 확인해볼까요?",
-                fontSize = 25.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.weight(1f)
-            )
-            Icon(
-                painter = painterResource(id = R.drawable.medal), // 메달 아이콘 리소스
-                contentDescription = "Medal",
-                modifier = Modifier.size(50.dp)
-            )
-        }
 
-        // 순위 리스트 (1~4위)
-        Box(
+        val (contentBoxBorder, contentBox) = createRefs()
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(Color(0xFFE0ECFF))
+                .fillMaxSize()
                 .padding(16.dp)
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+            // 상단 메달과 텍스트
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
             ) {
-                for (i in 1..4) {
-                    Text(
-                        text = "$i",
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Black
-                    )
-                }
+                Text(
+                    text = "순위를 확인해볼까요?",
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(1f)
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.medal),
+                    contentDescription = "Medal",
+                    modifier = Modifier.size(50.dp)
+                )
             }
         }
 
-        Spacer(modifier = Modifier.weight(1f)) // 남은 공간을 차지하는 Spacer 추가
-
-        // 나의 랭킹 (하단 고정)
-        Row(
+        // LazyColumn을 ConstraintLayout 내에서 직접 사용
+        LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .clip(RoundedCornerShape(8.dp))
-                .background(Color(0xFFF6F6F6))
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .height(screenHeight / 2)
+                .padding(start = 15.dp)
+                .constrainAs(contentBox) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top, margin = 100.dp) // Column과 LazyColumn 사이의 여백을 지정
+                    bottom.linkTo(parent.bottom)
+                }
         ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = "나의 랭킹",
-                    fontSize = 12.sp,
-                    color = Color.Gray
-                )
-                Text(
-                    text = "327",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
-            }
+            // 유사도 순으로 내림차순 정렬
+            val sortedRanks = userRank.sortedBy { it.try_cnt } ?: emptyList()
 
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = "도전 횟수",
-                    fontSize = 12.sp,
-                    color = Color.Gray
-                )
-                Text(
-                    text = "57회",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
+            items(sortedRanks.size) { index ->
+                val rank = sortedRanks[index]
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp)
+                        .height(IntrinsicSize.Min)
+                ) {
+                    Text(
+                        text = "${index + 1}",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.width(30.dp)
+                    )
+
+                    Text(
+                        text = rank.nickname,
+                        fontSize = 18.sp,
+                        maxLines = 2,
+                        softWrap = true,
+                        modifier = Modifier
+                            .width(IntrinsicSize.Max)
+                            .weight(1f)
+                            .padding(start = 8.dp)
+                    )
+
+                    Text(
+                        text = "${rank.try_cnt}",
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .width(100.dp)
+                            .padding(end = 52.dp)
+                            .alignBy { it.measuredHeight / 2 }
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/maker/pacemaker/ui/viewmodel/main/details/MainCSMantleRankingScreenViewModel.kt
+++ app/src/main/java/com/maker/pacemaker/ui/viewmodel/main/details/MainCSMantleRankingScreenViewModel.kt
@@ -3,8 +3,11 @@ package com.maker.pacemaker.ui.viewmodel.main.details
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.maker.pacemaker.data.model.remote.SimilarityWord
+import com.maker.pacemaker.data.model.remote.SolvedUser
 import com.maker.pacemaker.ui.viewmodel.main.MainBaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,6 +21,9 @@ open class MainCSMantleRankingScreenViewModel @Inject constructor(
 
     val repository = baseViewModel.repository
 
+    private val _userRanks = MutableStateFlow<List<SolvedUser>>(emptyList())
+    val userRanks = _userRanks
+
     fun showRanking() {
         viewModelScope.launch {
             val response = repository.getSolvedUsers()
@@ -25,9 +31,14 @@ open class MainCSMantleRankingScreenViewModel @Inject constructor(
             Log.d("MainCSMantleScreenViewModel", "Response: $response")
             response.forEach { user ->
                 Log.d("MainCSMantleScreenViewModel", "Try_cnt: ${user.try_cnt}, Nickname: ${user.nickname}, UID: ${user.uid}")
-            }
+                val try_cnt = user.try_cnt
+                val name = user.nickname
+                val userid = user.uid
 
+                val newRank = _userRanks.value + SolvedUser(userid, name, try_cnt)
+                _userRanks.value = newRank.sortedByDescending { try_cnt }
+
+            }
         }
     }
-
 }
