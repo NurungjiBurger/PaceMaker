068addc934a53ee89ecc900116eba00584cd564b
NurungjiBurger <qazw181900@naver.com>
2024-11-17 13:38:40 +0900

    [#63] 문제카드 UI 수정
    문제카드 UI 수정

--- app/src/main/java/com/maker/pacemaker/ui/screen/Component/ProblemCard.kt
+++ app/src/main/java/com/maker/pacemaker/ui/screen/Component/ProblemCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +15,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -32,6 +34,8 @@ fun ProblemCard(
 {
 
     Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth()
@@ -61,17 +65,6 @@ fun ProblemCard(
                 .padding(16.dp),
             fontSize = 30.sp,
             fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = problem.description,
-            modifier = Modifier
-                .padding(16.dp),
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Normal,
             color = Color.Black
         )
     }
