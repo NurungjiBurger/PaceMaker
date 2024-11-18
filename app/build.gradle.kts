fdaf2c21c361829031e3761c87a0d5f930651288
박상빈 <qazw181900@naver.com>
2024-11-18 12:10:56 +0900

    [chore] 버전 업
    버전 업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.maker.pacemaker"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
